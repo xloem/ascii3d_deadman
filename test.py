be0c7cf707eec8dad89f5639c30b84b570a69d5e
John Doe <johndoe@example.com>
2022-06-22 10:17:59 -0400

    wrapped in class, used width and height to center

--- test.py
+++ test.py
@@ -1,18 +1,44 @@
+#!/usr/bin/env python3
 import curses
 
-def main(stdscr):
-    stdscr.nodelay(True)
-    stdscr.clear()
+class Engine:
+    def run(self):
+        curses.wrapper(self.__run)
+    def __init(self):
+        self.window.nodelay(True)
+        self.window.clear()
+    def __run(self, window):
+        self.window = window
+        self.__init()
+        while True:
+            self.update(self.__update())
+    def plot(self, x, y, str):
+        line = round(y / self.char_height)
+        row = round(x / self.char_width)
+        self.window.addstr(line, row, str)
+    def __update(self):
+        # getting a key also refreshes
+        try:
+            key = self.window.getkey()
+        except:
+            key = ''
+        # wipe for next draw
+        self.cols = curses.COLS
+        self.lines = curses.LINES
+        self.char_width = 8
+        self.char_height = 16
+        self.width = self.cols * self.char_width
+        self.height = self.lines * self.char_height
+        self.window.erase()
+        return key
 
-    key = 'press key?'
+class App(Engine):
+    def __init__(self):
+        self.last_key = 'press key?'
+    def update(self, key = ''):
+        if key:
+            self.last_key = key
+        self.plot(self.width / 2, self.height / 2, self.last_key)
 
-    while key != 'q':
-      try:
-        key = stdscr.getkey()
-      except:
-        pass
-      stdscr.erase()
-      stdscr.addstr(4, 3, key)
-      #stdscr.refresh()
-
-curses.wrapper(main)
+if __name__ == '__main__':
+    App().run()
